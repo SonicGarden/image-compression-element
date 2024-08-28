681d3228ce1db8a4c7b0497203fb109aa83a5d67
aki <lala.akira@gmail.com>
2024-08-28 18:13:53 +0900

    First release

--- README.md
+++ README.md
@@ -1,49 +1,76 @@
-# &lt;custom-element&gt; element
+# &lt;image-compression&gt; element
 
-Boilerplate for creating a custom element.
+A custom element for compressing image files. This element automatically compresses images when they are uploaded via a file input field.
+
+## Features
+- Supports JPEG, PNG, WEBP formats
+- Customizable compression options
+- Supports events during the compression process
+- Uses `browser-image-compression` dynamically imported from a CDN, resulting in a smaller build size
 
 ## Installation
-Available on [npm](https://www.npmjs.com/) as [**@github/custom-element-element**](https://www.npmjs.com/package/@github/custom-element-element).
 ```
-$ npm install --save @github/custom-element-element
+$ npm install --save @sonicgarden/image-compression-element
 ```
 
 ## Usage
 
-### Script
+### Basic Example
 
 Import as ES modules:
 
 ```js
-import '@github/custom-element-element'
+import '@sonicgarden/image-compression-element'
 ```
-
-Include with a script tag:
 
 ```html
-<script type="module" src="./node_modules/@github/custom-element-element/dist/index.js">
+<image-compression
+  initial-quality="0.8"
+  max-size-mb="2"
+  max-width-or-height="1024"
+  use-web-worker>
+  <input type="file" accept="image/*" />
+</image-compression>
 ```
 
-An example Custom Element. This documentation ends up in the
-README so describe how this elements works here.
+### Attributes
+- `initial-quality`: Initial quality of the compressed image (default: 0.9)
+- `max-size-mb`: Maximum size of the compressed image in MB (default: 5)
+- `max-width-or-height`: Maximum width or height of the compressed image (default: 4096)
+- `use-web-worker`: Whether to use a Web Worker for the compression process (default: false)
 
-You can event add examples on the element is used with Markdown.
+### Events
+The `<image-compression>` element emits a change event when the image compression is complete. This event includes a `compression` property indicating whether the compression was successful.
 
+```js
+document.querySelector('image-compression').addEventListener('change', (event) => {
+  if (event.detail.compression) {
+    console.log('Image compression was successful');
+  } else {
+    console.log('Image compression failed');
+  }
+});
 ```
-<custom-element></custom-element>
+
+### Overriding Default Options
+
+```js
+import { ImageCompression } from '@sonicgarden/image-compression-element'
+
+// SEE: https://github.com/Donaldcwl/browser-image-compression?tab=readme-ov-file#main-function
+ImageCompression.defaultOptions = {
+  initialQuality: 0.9,
+  maxSizeMB: 2,
+  maxWidthOrHeight: 2048,
+  useWebWorker: true
+};
 ```
 
-## Browser support
+## dependencies
 
-Browsers without native [custom element support][support] require a [polyfill][].
-- Chrome
-- Firefox
-- Safari
-- Microsoft Edge
-
-[support]: https://caniuse.com/custom-elementsv1
-[polyfill]: https://github.com/webcomponents/custom-elements
+This custom element has no dependencies.
+The `browser-image-compression` library is dynamically imported from a CDN.
 
 ## License
 
-Distributed under the MIT license. See LICENSE for details.
+Distributed under the MIT license.
---
+++ src/image-compression.ts
@@ -0,0 +1,122 @@
+import type {Options} from 'browser-image-compression'
+
+const isSupportedImageCompression = (file: File): boolean => {
+  return ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'].includes(file.type)
+}
+
+const isSupportedDataTransfer = (): boolean => {
+  try {
+    new DataTransfer()
+    return true
+  } catch {
+    // NOTE: less than safari 14.1
+    return false
+  }
+}
+
+// NOTE: Hack to generate FileList
+// SEE: https://qiita.com/jkr_2255/items/1c30f7afefe6959506d2
+const filesToFileList = (files: readonly File[]): FileList => {
+  const dt = new DataTransfer()
+  files.map(blob => {
+    // NOTE: Explicitly cast because browser-image-compression returns a Blob extended with the File interface, not an actual File
+    dt.items.add(new File([blob], blob.name, {type: blob.type, lastModified: blob.lastModified}))
+  })
+  return dt.files
+}
+
+const imageCompressionByFileList = async (files: FileList, options: Options = {}): Promise<FileList> => {
+  // eslint-disable-next-line import/extensions, import/no-unresolved
+  const imageCompression = (await import('https://esm.run/browser-image-compression@2.0.2')).default
+  const promises = [...files].map(file => {
+    return isSupportedImageCompression(file) ? imageCompression(file, options) : file
+  })
+  const compressedFiles = await Promise.all(promises)
+  return filesToFileList(compressedFiles)
+}
+
+export type ImageCompressionEventDetail = {
+  compression: boolean
+}
+
+export class ImageCompression extends HTMLElement {
+  static defaultOptions: Options = {
+    maxWidthOrHeight: 4096,
+    maxSizeMB: 5,
+    initialQuality: 0.9,
+  }
+
+  connectedCallback() {
+    this.addEventListener('change', this.onChange)
+  }
+
+  disconnectedCallback(): void {
+    this.removeEventListener('change', this.onChange)
+  }
+
+  onChange = async (event: Event | CustomEvent<ImageCompressionEventDetail>) => {
+    if (!isSupportedDataTransfer()) return
+    if ('detail' in event && event.detail.compression !== undefined) return
+
+    const {target} = event
+    if (!(target instanceof HTMLInputElement) || !target.files || target.files.length === 0) return
+
+    event.preventDefault()
+    event.stopPropagation()
+
+    target.disabled = true
+    let compression = false
+
+    try {
+      target.files = await imageCompressionByFileList(target.files, this.options)
+      compression = true
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn(error)
+    }
+
+    target.disabled = false
+
+    target.dispatchEvent(new CustomEvent<ImageCompressionEventDetail>('change', {bubbles: true, detail: {compression}}))
+  }
+
+  get options(): Options {
+    return {
+      ...ImageCompression.defaultOptions,
+      initialQuality: this.initialQuality ?? ImageCompression.defaultOptions.initialQuality,
+      maxSizeMB: this.maxSizeMB ?? ImageCompression.defaultOptions.maxSizeMB,
+      maxWidthOrHeight: this.maxWidthOrHeight ?? ImageCompression.defaultOptions.maxWidthOrHeight,
+      useWebWorker: this.useWebWorker ?? ImageCompression.defaultOptions.useWebWorker,
+    }
+  }
+
+  get initialQuality(): Options['initialQuality'] {
+    const value = this.getAttribute('initial-quality')
+    return value ? Number.parseFloat(value) : undefined
+  }
+
+  get maxSizeMB(): Options['maxSizeMB'] {
+    const value = this.getAttribute('max-size-mb')
+    return value ? Number.parseInt(value, 10) : undefined
+  }
+
+  get maxWidthOrHeight(): Options['maxWidthOrHeight'] {
+    const value = this.getAttribute('max-width-or-height')
+    return value ? Number.parseInt(value, 10) : undefined
+  }
+
+  get useWebWorker(): Options['useWebWorker'] {
+    return this.hasAttribute('use-web-worker') || undefined
+  }
+}
+
+declare global {
+  interface Window {
+    ImageCompression: typeof ImageCompression
+  }
+}
+
+if (!window.customElements.get('image-compression')) {
+  window.ImageCompression = ImageCompression
+  window.customElements.define('image-compression', ImageCompression)
+}
